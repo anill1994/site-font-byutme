57238e786769f3851fadab0d1737d93a96396637
Anil Aydin <anil_94-@hotmail.com>
2020-06-19 22:20:13 +0300

    ilk commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var say = 16;
+function buyut(){
+	say += 1;
+	var alan  =document.getElementById("alan").style.fontSize = say + "px"
+}
+function kucult(){
+	say -= 1;
+	var alan  =document.getElementById("alan").style.fontSize = say + "px"
+}
+
